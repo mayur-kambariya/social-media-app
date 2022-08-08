4fe51e9434beb56d835b5645e1347c394286451d
Mayur Kambariya <mayurkambariya@gmail.com>
2022-08-08 11:40:37 +0530

    initial commit with Login and Registration using devise gem

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'dashboard/index'
+  devise_for :users
+
+  root "dashboard#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
